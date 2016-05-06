113a43556a4aa94f0f3a8217198b555655c53096
Fadul <fadul.fatouma@gmail.com>
2016-05-06 13:57:04 +0200

    FIX
    Correction of the method SendInt

--- src/Fork.java
+++ src/Fork.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class Fork {
 
+	static int nb1 = 0;
+	static int nb2 = 0;
+	static int nb3 = 0;
+	static int nbuser = 0;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		int nbuser = 0;
+
 		// Read Keyboarb
 		while (nbuser != -1) {
 			Scanner readKeyb = new Scanner(System.in);
@@ -18,25 +24,19 @@ public class Fork {
 	}
 
 	public static String SendInt(int nbuser) {
-		int nb1 = 0;
-		int nb2 = 0;
-		int nb3 = 0;
-		for (int i = 0; i >= 0; i++) {
-			if (nbuser > nb1) {
-				nb3 = nb2;
-				nb2 = nb1;
-				nb1 = nbuser;
-			}
-			if (nbuser > nb2 && nbuser < nb1) {
-				nb3 = nb2;
-				nb2 = nbuser;
-			}
-			if (nbuser > nb3 && nbuser < nb2) {
-				nb3 = nbuser;
-			}
-			String str = nb1 + ", " + nb2 + " and " + nb3;
-			return str;
+		if (nbuser > nb1) {
+			nb3 = nb2;
+			nb2 = nb1;
+			nb1 = nbuser;
 		}
-		return "end";
+		if (nbuser > nb2 && nbuser < nb1) {
+			nb3 = nb2;
+			nb2 = nbuser;
+		}
+		if (nbuser > nb3 && nbuser < nb2) {
+			nb3 = nbuser;
+		}
+		String str = nb1 + ", " + nb2 + " and " + nb3;
+		return str;
 	}
 }
